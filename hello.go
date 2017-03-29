acabc9a31ea8ee28993cc184c636cb22f58456f2
mnwalalm <martin-nwalal-mi-nyom@capgemini.com>
2017-03-29 14:04:54 +0100

    amending  the go code

--- hello.go
+++ hello.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", hello)
+	http.HandleFunc("/", hello )
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal("Failed to start server: ", err)
@@ -14,5 +14,5 @@ func main() {
 }
 
 func hello(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello From Golang"))
+	w.Write([]byte("Hello From Golang and from Martin"))
 }
